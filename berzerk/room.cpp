235db2f788685388f30fb977e5ae07e5b47a36fd
Billy <billymcfly@gmail.com>
2020-04-04 15:05:07 -0600

    Add walls inside doors

--- berzerk/room.cpp
+++ berzerk/room.cpp
@@ -169,17 +169,31 @@ void Room::CreateWalls()
 {
 	const sf::Vector2f topLeft(48, 48);
 	// Left
+	// Above and below door
 	entityManager->Add(new EntityWall(sf::Vector2f(topLeft.x - WALL_THICKNESS, topLeft.y), sf::Vector2f(WALL_THICKNESS, WALL_HEIGHT), sf::Color::Red));
 	entityManager->Add(new EntityWall(sf::Vector2f(topLeft.x - WALL_THICKNESS, topLeft.y + WALL_HEIGHT + DOOR_WIDTH), sf::Vector2f(WALL_THICKNESS, WALL_HEIGHT), sf::Color::Red));
+	// Inside door area
+	entityManager->Add(new EntityWall(sf::Vector2f(topLeft.x - WALL_WIDTH, topLeft.y + WALL_HEIGHT), sf::Vector2f(WALL_WIDTH, WALL_THICKNESS), sf::Color::Red));
+	entityManager->Add(new EntityWall(sf::Vector2f(topLeft.x - WALL_WIDTH, topLeft.y + WALL_HEIGHT + DOOR_WIDTH), sf::Vector2f(WALL_WIDTH, WALL_THICKNESS), sf::Color::Red));
+
 	// Top
 	entityManager->Add(new EntityWall(sf::Vector2f(topLeft.x, topLeft.y - WALL_THICKNESS), sf::Vector2f(WALL_WIDTH, WALL_THICKNESS), sf::Color::Red));
 	entityManager->Add(new EntityWall(sf::Vector2f(topLeft.x + WALL_WIDTH + DOOR_WIDTH, topLeft.y - WALL_THICKNESS), sf::Vector2f(WALL_WIDTH, WALL_THICKNESS), sf::Color::Red));
+
+	entityManager->Add(new EntityWall(sf::Vector2f(topLeft.x + WALL_WIDTH - WALL_THICKNESS, topLeft.y - WALL_HEIGHT), sf::Vector2f(WALL_THICKNESS, WALL_HEIGHT), sf::Color::Red));
+	entityManager->Add(new EntityWall(sf::Vector2f(topLeft.x + WALL_WIDTH + DOOR_WIDTH, topLeft.y - WALL_HEIGHT), sf::Vector2f(WALL_THICKNESS, WALL_HEIGHT), sf::Color::Red));
 	// Right
 	entityManager->Add(new EntityWall(sf::Vector2f(topLeft.x + (WALL_WIDTH * 2) + DOOR_WIDTH, topLeft.y), sf::Vector2f(WALL_THICKNESS, WALL_HEIGHT), sf::Color::Red));
 	entityManager->Add(new EntityWall(sf::Vector2f(topLeft.x + (WALL_WIDTH * 2) + DOOR_WIDTH, topLeft.y + WALL_HEIGHT + DOOR_WIDTH), sf::Vector2f(WALL_THICKNESS, WALL_HEIGHT), sf::Color::Red));
+
+	entityManager->Add(new EntityWall(sf::Vector2f(topLeft.x + (WALL_WIDTH * 2) + DOOR_WIDTH, topLeft.y + WALL_HEIGHT), sf::Vector2f(WALL_WIDTH, WALL_THICKNESS), sf::Color::Red));
+	entityManager->Add(new EntityWall(sf::Vector2f(topLeft.x + (WALL_WIDTH * 2) + DOOR_WIDTH, topLeft.y + WALL_HEIGHT + DOOR_WIDTH), sf::Vector2f(WALL_WIDTH, WALL_THICKNESS), sf::Color::Red));
 	// Bottom
 	entityManager->Add(new EntityWall(sf::Vector2f(topLeft.x, (topLeft.y + WALL_HEIGHT * 2) + DOOR_WIDTH), sf::Vector2f(WALL_WIDTH, WALL_THICKNESS), sf::Color::Red));
 	entityManager->Add(new EntityWall(sf::Vector2f(topLeft.x + WALL_WIDTH + DOOR_WIDTH, (topLeft.y + WALL_HEIGHT * 2) + DOOR_WIDTH), sf::Vector2f(WALL_WIDTH, WALL_THICKNESS), sf::Color::Red));
+
+	entityManager->Add(new EntityWall(sf::Vector2f(topLeft.x + WALL_WIDTH - WALL_THICKNESS, (topLeft.y + WALL_HEIGHT * 2) + DOOR_WIDTH), sf::Vector2f(WALL_THICKNESS, WALL_HEIGHT), sf::Color::Red));
+	entityManager->Add(new EntityWall(sf::Vector2f(topLeft.x + WALL_WIDTH + DOOR_WIDTH, (topLeft.y + WALL_HEIGHT * 2) + DOOR_WIDTH), sf::Vector2f(WALL_THICKNESS, WALL_HEIGHT), sf::Color::Red));
 }
 
 void Room::CreateDoors()
