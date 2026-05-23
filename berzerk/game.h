14d763d4b5ae660b8c395c267ecffd776873f40c
Billy McPherson <william.d.mcpherson@gmail.com>
2026-05-23 16:17:27 -0600

    Change game width to 640

--- berzerk/game.h
+++ berzerk/game.h
@@ -6,7 +6,7 @@
 #include "anim_manager.h"
 
 #define GAME_NAME "Bazoik"
-#define GAME_WIDTH 848
+#define GAME_WIDTH 640
 #define GAME_HEIGHT 480
 #define START_LIVES 3
 #define EXTRA_LIFE_SCORE 1000 // Extra life is awarded every x points
